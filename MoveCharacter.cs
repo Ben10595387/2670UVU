36f93bdfaa702cd5e419d466f76700aa0fbf2f10
Ben10595387 <10595387@my.uvu.edu>
2017-08-28 14:23:28 -0600

    Classwork 8/28

---
+++ MoveCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(CharacterController))]
+
+
+
+public class MoveCharacter : MonoBehaviour {
+
+    CharacterController cc;
+    
+	
+	void Start () {
+        cc = GetComponent<CharacterController>();
+        moveinput.KeyAction = Move; 
+		
+	}
+	
+	// Update is called once per frame
+	void Move (float _movement) {
+        print(_movement);
+   
+		}
+}
---
+++ moveinput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class moveinput : MonoBehaviour {
+
+
+	public static Action<float> KeyAction;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (KeyAction != null)
+		{
+			KeyAction(Input.GetAxis("Horizontal"));
+		}
+		
+	}
+}
