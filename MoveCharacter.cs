4ee029472d28a00db52997b9462cc302be558bb3
Ben10595387 <10595387@my.uvu.edu>
2017-09-03 23:21:05 -0600

    Add files via upload

--- MoveCharacter.cs
+++ MoveCharacter.cs
@@ -9,17 +9,27 @@ using System;
 
 public class MoveCharacter : MonoBehaviour {
 
-    CharacterController cc;
+	public static Action<float> KeyAction;
+	
+	public static Action<bool> JumpAction;
 
-	Vector3 tempmove;
+	CharacterController cc;
 
+	Vector3 tempmove = Vector3.zero;
+	
 	public float speed = 5;
 
 	public float gravity = 1;
 
 	public float jumpHeight = 0.2f;
+
+	public float jumpAmount = 0.0f;
+
+	float maxJumpCount = 2.0f;
+	
     
 	void Start () {
+		
 		cc = GetComponent<CharacterController>();
 		PlayButton.pushedPlay += OnPlay;
 
@@ -28,25 +38,36 @@ public class MoveCharacter : MonoBehaviour {
 	void OnPlay () {
         cc = GetComponent<CharacterController>();
         moveinput.KeyAction += Move; 
-		moveinput.KeyAction += Jump;
 		PlayButton.pushedPlay -= OnPlay;
 		//to make the character have the animation as
 		//well as this script work. It must be += instead of just = Move on both scripts. 
 		
 	}
 
-	void Jump() {
-		print("jump");
-		tempmove.y = jumpHeight;
-	}
-	
-	// Update is called once per frame
 	void Move (float _movement) {
        // print(_movement);
-		tempmove.y = gravity*Time.deltaTime;
-		tempmove.x =_movement*speed*Time.deltaTime;
+		tempmove.y -= gravity*Time.deltaTime;
+		tempmove.x =_movement*speed*Time.deltaTime * speed;
 		cc.Move(tempmove);
 		//script is only recieving float
    
 		}
+
+	void Jump(bool obj) {
+		if (!cc.isGrounded) {
+			tempmove.x = Input.GetAxis("Horizontal")*speed;
+		}
+		else {
+			tempmove = new Vector3(Input.GetAxis("Horizontal")*speed, 0.0f, Input.GetAxis("Vertical")*speed);
+			jumpAmount = 0.0f;
+		}
+
+		if (jumpAmount < maxJumpCount){
+			if (Input.GetKeyDown ("Jump")) {
+				tempmove.y += jumpHeight;
+				jumpAmount = 2.0f;
+			}
+		}
+		//print("Jumped");
+	}
 }
--- moveinput.cs
+++ moveinput.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+using System; //namespace system contains actions//
 
 public class moveinput : MonoBehaviour {
 
 
 	public static Action<float> KeyAction;
+	public static Action JumpAction;
 
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space) )
+	{
+			JumpAction();
+	}
+
 		if (KeyAction != null)
 		{
 			KeyAction(Input.GetAxis("Horizontal"));
